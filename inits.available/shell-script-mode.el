2b263e5b0a33fec0152f6d31d8207674630c544a
Shinya Ishida <shinyaishida@users.noreply.github.com>
2018-02-13 23:13:47 +0900

    Modified the name.

--- inits.available/shell-script-mode.el
+++
@@ -1,5 +0,0 @@
-(add-hook 'shell-script-mode-hook
-	  '(lambda()
-	     ;; This short-cut key is orginarlly bound to index variable.
-	     (local-set-key "\C-c\C-l" 'goto-line)
-	     (setq fill-column 80)))
