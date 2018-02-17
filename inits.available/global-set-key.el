b3bf927ac3ebd517b3c749ba7e1eb1f4049689a2
Shinya Ishida <shinyaishida@users.noreply.github.com>
2018-02-18 07:28:35 +0900

    Migrated functions.

--- inits.available/global-set-key.el
+++ inits.available/global-set-key.el
@@ -26,23 +26,6 @@
 (global-set-key (kbd "C-c l") 'linum-mode)
 (global-set-key (kbd "C-c L") 'goto-line)
 
-
-(defun indent-buffer ()
-  "Indent the currently visited buffer."
-  (interactive)
-  (indent-region (point-min) (point-max)))
-(defun indent-region-or-buffer ()
-  "Indent a region if selected, otherwise the whole buffer."
-  (interactive)
-  (save-excursion
-    (if (region-active-p)
-        (progn
-          (indent-region (region-beginning) (region-end))
-          (message "Indented selected region."))
-      (progn
-        (indent-buffer)
-        (message "Indented buffer.")))))
-
 (global-set-key (kbd "C-c C-f") 'load-file)
 (global-set-key (kbd "C-c C-e") 'eval-region)
 (global-set-key (kbd "C-c c") 'compile)
--- inits.available/my-func.el
+++ inits.available/my-func.el
@@ -3,9 +3,22 @@
   (interactive)
   (load-file "~/.emacs"))
 
-;;
-;; quoted from http://www.emacswiki.org/emacs/BackwardDeleteWord
-;;
+(defun indent-buffer ()
+  "Indent the currently visited buffer."
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun indent-region-or-buffer ()
+  "Indent a region if selected, otherwise the whole buffer."
+  (interactive)
+  (save-excursion
+    (if (region-active-p)
+        (progn
+          (indent-region (region-beginning) (region-end))
+          (message "Indented selected region."))
+      (progn
+        (indent-buffer)
+        (message "Indented buffer.")))))
 
 (defun delete-word (arg)
   "Delete characters forward until encountering the end of a word.
