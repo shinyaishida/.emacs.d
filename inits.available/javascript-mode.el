c0c78f3337a115ec147fa7ced3bdb6bfa7a24e8f
Shinya Ishida <shinyaishida@users.noreply.github.com>
2018-02-25 23:25:14 +0900

    Use js2-mode.

--- inits.available/javascript-mode.el
+++
@@ -1,3 +0,0 @@
-(setq auto-mode-alist
-      (cons (cons "\\.js$" 'javascript-mode) auto-mode-alist))
-(autoload 'javascript-mode "javascript-mode" "Java Script" t)
---
+++ inits.available/js2-mode.el
@@ -0,0 +1,6 @@
+(use-package js2-mode
+  :ensure t
+  :defer t
+  :mode (("\\.js$" . js2-mode))
+  :config
+  (setq js2-basic-offset 2))
